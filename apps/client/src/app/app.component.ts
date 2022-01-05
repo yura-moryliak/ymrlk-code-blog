00d61ca396ef127ff86fe0ba5fc48fa4b6e577b0
yuramoryliak <y.moryliak@crosssoft.de>
2022-01-05 16:11:20 +0200

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ymrlk-code-blog-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}

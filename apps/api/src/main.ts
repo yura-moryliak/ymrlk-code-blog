828118f06a2d2c5cd118d8203dc46bda98cd53eb
yuramoryliak <y.moryliak@crosssoft.de>
2022-01-19 00:10:09 +0200

    MASTER  - change host connection

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );

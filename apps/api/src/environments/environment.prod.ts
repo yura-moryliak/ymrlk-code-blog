ba68eef08923b29e50bdcd116ea84ff74ea506ae
yuramoryliak <y.moryliak@crosssoft.de>
2022-01-22 15:11:43 +0200

    0010-client-setup-with-enhancement  - add env props

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+  server: {
+    host: 'https://ymrlk-code-blog.herokuapp.com',
+    port: process.env.PORT,
+    hostname: '0.0.0.0',
+    globalPrefix: 'api',
+    cors: { origin: 'https://ymrlk-code-blog.herokuapp.com' }
+  }
 };
